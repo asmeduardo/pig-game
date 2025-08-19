7a6a3f6761f589e50fa13a3731ea66613d1d0713
Eduardo Melo <eduardo.melo01992@gmail.com>
2025-08-19 13:54:30 -0300

    feat: adiciona funcionalidade de rolar dado e exibir imagem

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const dice = document.querySelector('.dice');
+const btnRollDice = document.querySelector('.btn--roll');
+
+const randomNumber = () => Math.floor(Math.random() * 6) + 1;
+
+const displayDice = function (number) {
+  dice.src = `assets/images/dice-${number}.png`;
+};
+
+btnRollDice.addEventListener('click', function () {
+  const number = randomNumber();
+  displayDice(number);
+});
